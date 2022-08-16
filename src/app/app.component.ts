0c729c6c98b826cdee5f12ef6edfd410d0125338
Md Anas Sabah <mdanassabah@gmail.com>
2022-08-16 21:55:42 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  list: any[] = [];
+
+  addTask(item: string) {
+    this.list.push({ id: this.list.length, name: item });
+    console.log(this.list);
+  }
+  removeTask(item: number) {
+    this.list.pop();
+  }
 }
